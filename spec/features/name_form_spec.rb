47fd2ffd3514a131db3a2c026f3a27b82bc429ce
CKKH <chriskkhassan@gmail.com>
2018-12-13 14:38:46 +0000

    DRY test code with test helpers

--- spec/features/name_form_spec.rb
+++ spec/features/name_form_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Chris'
-    fill_in :player_2_name, with: 'Dan'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Chris vs Dan'
   end
 end
--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'see hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Chris'
-    fill_in :player_2_name, with: 'Dan'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Chris hit points: 100'
   end
 end
