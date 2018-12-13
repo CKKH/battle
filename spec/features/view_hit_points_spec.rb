3ce7b584fc5f2274b2c7eaa54ca570f403794dc6
CKKH <chriskkhassan@gmail.com>
2018-12-13 14:26:51 +0000

    Viewing player 1 hit points implemented

---
+++ spec/features/view_hit_points_spec.rb
@@ -0,0 +1,9 @@
+feature 'View hit points' do
+  scenario 'see hit points' do
+    visit('/')
+    fill_in :player_1_name, with: 'Chris'
+    fill_in :player_2_name, with: 'Dan'
+    click_button 'Submit'
+    expect(page).to have_content 'Player 1 hit points: 100'
+  end
+end
